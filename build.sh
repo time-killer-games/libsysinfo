5e18aa91aca8f2d5ff9f2561f6a1beee35344b24
Samuel Venable <samuelvenable@hotmail.com>
2023-05-26 20:45:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 cd "${0%/*}"
 if [ $(uname) = "Darwin" ]; then
-  clang++ gamemaker.cpp system.cpp libGLEW.a libglfw3.a -o libsysinfo.dylib -std=c++17 -DGLEW_STATIC -Iinclude `pkg-config --cflags glfw3` `pkg-config --cflags glew` -framework Cocoa -framework IOKit -framework OpenGL -lobjc -arch arm64 -arch x86_64 -shared;
+  clang++ gamemaker.cpp system.cpp libGLEW.a libglfw3.a -o libsysinfo.dylib -std=c++17 -DGLEW_STATIC -Iinclude `pkg-config --cflags glfw3` `pkg-config --cflags glew` -framework Cocoa -framework IOKit -framework OpenGL -lobjc -arch arm64 -arch x86_64 -fPIC -shared;
 elif [ $(uname) = "Linux" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -fPIC -shared;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lkvm -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lkvm -fPIC -shared;
 elif [ $(uname) = "DragonFly" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lkvm -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -lkvm -fPIC -shared;
 elif [ $(uname) = "NetBSD" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -fPIC -shared;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -fPIC -shared;
 elif [ $(uname) = "SunOS" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs glfw3` `pkg-config --cflags --libs glew` -lGL -fPIC -shared;
 else
-  g++ gamemaker.cpp system.cpp -o libsysinfo.dll -std=c++17 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglfw3 -lglew32 -lopengl32 -lgdi32 -ld3d11 -ldxgi -shared;
+  g++ gamemaker.cpp system.cpp -o libsysinfo.dll -std=c++17 -DGLEW_STATIC -static-libgcc -static-libstdc++ -static -lws2_32 -lglfw3 -lglew32 -lopengl32 -lgdi32 -ld3d11 -ldxgi -fPIC -shared;
 fi
-
