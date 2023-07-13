3629683005da13324835f155e9adb7991412af8c
Samuel Venable <samuelvenable@hotmail.com>
2023-07-13 01:57:15 -0400

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -98,6 +98,8 @@ static bool create_context() {
     SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    EGLContext eglctx = eglGetCurrentContext();
+    if (eglctx != EGL_NO_CONTEXT) return;
     #endif
     window = SDL_CreateWindow("", 0, 0, 1, 1, SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN);
     if (!window) return false;
