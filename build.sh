c201e75bbc61c560c7b877bec56827432e743ebc
Samuel Venable <samuelvenable@hotmail.com>
2023-07-13 03:54:17 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ if [ $(uname) = "Darwin" ]; then
   lipo -create -output ../libEGL.dylib ../arm64/libEGL.dylib ../x86_64/libEGL.dylib && lipo -create -output ../libGLESv2.dylib ../arm64/libGLESv2.dylib ../x86_64/libGLESv2.dylib && lipo -create -output ../libc++_chrome.dylib ../arm64/libc++_chrome.dylib ../x86_64/libc++_chrome.dylib && lipo -create -output ../libchrome_zlib.dylib ../arm64/libchrome_zlib.dylib ../x86_64/libchrome_zlib.dylib && lipo -create -output ../libthird_party_abseil-cpp_absl.dylib ../arm64/libthird_party_abseil-cpp_absl.dylib ../x86_64/libthird_party_abseil-cpp_absl.dylib;
   cd ..; clang++ /opt/local/lib/libSDL2.a gamemaker.cpp system.cpp -o libsysinfo.dylib -Iinclude -L. -I/opt/local/include -I/opt/local/include/SDL2 -std=c++17 -DCREATE_CONTEXT -lobjc -liconv -lEGL -lGLESv2 -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-weak_framework,CoreHaptics -Wl,-weak_framework,GameController -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal -fPIC -arch x86_64 -arch arm64 -fPIC -shared; install_name_tool -change @rpath/libEGL.dylib @loader_path/libEGL.dylib ./libsysinfo.dylib; install_name_tool -change @rpath/libGLESv2.dylib @loader_path/libGLESv2.dylib ./libsysinfo.dylib;
 elif [ $(uname) = "Linux" ]; then
-  g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -DCREATE_CONTEXT -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
+  g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
 elif [ $(uname) = "FreeBSD" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -DCREATE_CONTEXT `pkg-config --cflags --libs sdl2` -lGL -lkvm -fPIC -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs sdl2` -lGL -lkvm -fPIC -shared;
 elif [ $(uname) = "DragonFly" ]; then
-  g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -DCREATE_CONTEXT -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` -lGL -lkvm -fPIC -shared;
+  g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` -lGL -lkvm -fPIC -shared;
 elif [ $(uname) = "NetBSD" ]; then
-  g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -DCREATE_CONTEXT -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
+  g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -DCREATE_CONTEXT `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
 elif [ $(uname) = "SunOS" ]; then
-  export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig && g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -DCREATE_CONTEXT -static-libgcc `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
+  export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig && g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -static-libgcc `pkg-config --cflags --libs sdl2` -lGL -fPIC -shared;
 else
   g++ gamemaker.cpp system.cpp -o libsysinfo.dll -std=c++17 -DCREATE_CONTEXT -static-libgcc -static-libstdc++ -static `pkg-config --cflags --libs --static sdl2` -lws2_32 -ldxgi -lopengl32 -static -fPIC -shared;
 fi
