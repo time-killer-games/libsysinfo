c984a4011287c6c5e5c04e125ce3b3f0b512ea3c
Samuel Venable <samuelvenable@hotmail.com>
2023-07-13 01:32:00 -0400

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -106,7 +106,7 @@ static bool create_context() {
     if (!context) return false;
     int err = SDL_GL_MakeCurrent(window, context);
     if (err) return false;
-    int err = SDL_GL_MakeCurrent(window, previous);
+    err = SDL_GL_MakeCurrent(window, previous);
     if (err) return false;
     SDL_GL_DeleteContext(context);
   }
