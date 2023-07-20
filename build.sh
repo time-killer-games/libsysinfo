f8d048033b244ff25d07ff8f17c246dbb6d51b42
Samuel Venable <samuelvenable@hotmail.com>
2023-07-20 12:29:37 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd "${0%/*}"
+cd "${0%/*}";
+xxd -i pci.ids > pci.ids.hpp;
 if [ $(uname) = "Darwin" ]; then
   clang++ gamemaker.cpp system.cpp -o libsysinfo.dylib -std=c++17 -fPIC -shared;
 elif [ $(uname) = "Linux" ]; then
