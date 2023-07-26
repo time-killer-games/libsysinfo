e3eb8e69e8a16eb46becdce0bffaec704b7d3d59
Samuel Venable <samuelvenable@hotmail.com>
2023-07-26 00:38:33 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd "${0%/*}";
 xxd -i pci.ids > pci.ids.hpp;
 if [ $(uname) = "Darwin" ]; then
-  clang++ gamemaker.cpp system.cpp -o libsysinfo.dylib -std=c++17 -fPIC -shared;
+  clang++ gamemaker.cpp system.cpp -o libsysinfo.dylib -std=c++17 -fPIC -shared -arch x86_64 -arch arm64;
 elif [ $(uname) = "Linux" ]; then
   g++ gamemaker.cpp system.cpp -o libsysinfo.so -std=c++17 -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2` `pkg-config --cflags --libs x11` -lGL -fPIC -shared;
 elif [ $(uname) = "FreeBSD" ]; then
